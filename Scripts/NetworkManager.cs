ce1735b0ae1830573e6556ffc6396487b81993df
항상 졸린 연두 <108284815+yeondu1062@users.noreply.github.com>
2025-05-07 00:38:35 +0900

    Add files via upload

--- Scripts/NetworkManager.cs
+++ Scripts/NetworkManager.cs
@@ -1,18 +1,17 @@
 ﻿/* ____ _                   _____ ____  
-  / ___| |__   ___  ___ ___|___ /|  _ \ 
- | |   | '_ \ / _ \/ __/ __| |_ \| | | |
- | |___| | | |  __/\__ \__ \___) | |_| |
-  \____|_| |_|\___||___/___/____/|____/ 
-    written by @yeondu1062.
+ / ___| |__   ___  ___ ___|___ /|  _ \ 
+| |   | '_ \ / _ \/ __/ __| |_ \| | | |
+| |___| | | |  __/\__ \__ \___) | |_| |
+ \____|_| |_|\___||___/___/____/|____/ 
+   written by @yeondu1062.
 */
 
 using UnityEngine;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Threading;
-using System.Text;
-using System.Net.Sockets;
 using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System;
 
 public class NetworkManager : MonoBehaviour
@@ -20,91 +19,78 @@ public class NetworkManager : MonoBehaviour
     public static NetworkManager instance { get; private set; }
 
     private const int PORT = 51062;
-    private bool isServerRunning = false;
+    private const string MAGIC = "C1062;";
+
     private UdpClient udpClient;
-    private UdpClient udpServer;
+
+    private readonly Queue<Action> mainThreadQueue = new();
+
+    private ServerLoop serverLooper;
+    private ClientLoop clientLooper;
 
     private void Awake()
     {
         if (instance != null && instance != this) Destroy(gameObject);
         instance = this;
 
+        serverLooper = new ServerLoop(ScheduleMainThread, PORT, MAGIC);
+        clientLooper = new ClientLoop(ScheduleMainThread, PORT, MAGIC);
+
         udpClient = new UdpClient();
         udpClient.EnableBroadcast = true;
 
-        StartServer();
+        serverLooper.Start();
     }
 
-    private void StartServer()
+    private void Update()
     {
-        if (isServerRunning) return;
-        isServerRunning = true;
-
-        Thread thread = new Thread(ServerLoop);
-        thread.IsBackground = true;
-        thread.Start();
-    }
-
-    private void ServerLoop()
-    {
-        udpServer = new UdpClient(PORT);
-        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
-
-        while (isServerRunning)
+        lock (mainThreadQueue) 
         {
-            if (udpServer.Available > 0 && Encoding.UTF8.GetString(udpServer.Receive(ref remoteEP)) == "C1062;Discovery")
-            {
-                byte[] res = Encoding.UTF8.GetBytes("C1062;DiscoveryRes");
-                udpServer.Send(res, res.Length, remoteEP);
-            }
+            while (mainThreadQueue.Count > 0) mainThreadQueue.Dequeue().Invoke();
         }
     }
 
-    private void StopServer()
+    private void ScheduleMainThread(Action action)
     {
-        isServerRunning = false;
-
-        udpServer.Close();
-        udpServer.Dispose();
-        udpServer = null;
+        lock (mainThreadQueue) mainThreadQueue.Enqueue(action);
     }
 
     public async Task<List<IPEndPoint>> FindServers()
     {
-        byte[] discovery = Encoding.UTF8.GetBytes("C1062;Discovery");
+        byte[] discovery = Encoding.UTF8.GetBytes(MAGIC + "Discovery");
         udpClient.Send(discovery, discovery.Length, new IPEndPoint(IPAddress.Broadcast, PORT));
-        
-        return await WaitForResponses(2000);
+
+        return await clientLooper.WaitForResponses(udpClient, 1500);
     }
 
-    private async Task<List<IPEndPoint>> WaitForResponses(int timeout)
+    public void JoinServer(string ip)
     {
-        List<IPEndPoint> servers = new List<IPEndPoint>();
-        var startTime = DateTime.UtcNow;
+        byte[] join = Encoding.UTF8.GetBytes(MAGIC + "Join");
+        udpClient.Send(join, join.Length, new IPEndPoint(IPAddress.Parse(ip), PORT));
 
-        while ((DateTime.UtcNow - startTime).TotalMilliseconds < timeout)
-        {
-            if (udpClient != null && udpClient.Available > 0)
-            {
-                UdpReceiveResult result = await udpClient.ReceiveAsync();
-                string msg = Encoding.UTF8.GetString(result.Buffer);
+        ChessManager.instance.InitGame();
+        ChessManager.instance.playerType = 2;
+        FindFirstObjectByType<JoinMsgUi>().Show(ip);
 
-                if (msg == "C1062;DiscoveryRes" && !servers.Contains(result.RemoteEndPoint))
-                {
-                    servers.Add(result.RemoteEndPoint);
-                }
-            }
-            await Task.Delay(10);
-        }
+        serverLooper.Stop();
+        clientLooper.Start(ip, udpClient);
+    }
 
-        //if (servers.Count > 0) servers.RemoveAt(0); //Remove local IP
-        return servers;
+    public void MoveDataToServer(float fromX, float fromZ, float toX, float toZ)
+    {
+        byte[] move = Encoding.UTF8.GetBytes(MAGIC + $"Move;{fromX};{fromZ};{toX};{toZ};");
+        clientLooper.SendToServer(move);
+    }
+
+    public void MoveDataToClient(float fromX, float fromZ, float toX, float toZ)
+    {
+        byte[] move = Encoding.UTF8.GetBytes(MAGIC + $"Move;{fromX};{fromZ};{toX};{toZ};");
+        serverLooper.SendToClient(move);
     }
 
     private void OnApplicationQuit()
     {
-        StopServer();
-        udpClient.Close();
-        udpClient.Dispose();
+        serverLooper.Stop();
+        clientLooper.Stop();
     }
 }
